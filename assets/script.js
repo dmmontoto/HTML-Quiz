56a09918e950407e8ab6491bc7f6ca249f104bd4
dmmontoto <dmontoto@ucsd.edu>
2023-07-12 10:37:57 -0700

    Created JS object holding all 10 questions

---
+++ assets/script.js
@@ -0,0 +1,94 @@
+// Create an object that contains all Quiz Questions 
+
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        answerA: "Hyper Text Markup Language",
+        answerB: "Hyper Tool Markup Language",
+        answerC: "Home Text Modern Language",
+        answerD: "High Tech Manipulation Language",
+        correctAnswer: "A"
+    },
+
+    {
+        question: "What is the correct HTML element for the largest heading?",
+        answerA: "<p>",
+        answerB: "<title>",
+        answerC: "<h1>",
+        answerD: "<h*>",
+        correctAnswer: "C"
+    },
+
+    {
+        question: "How can you make a bulleted list in HTML?",
+        answerA: "<ol>",
+        answerB: "<bp>",
+        answerC: "<a>",
+        answerD: "<ul>",
+        correctAnswer: "D",
+    },
+
+    {
+        question: "What is the correct way to create HTML in-line comments?",
+        answerA: "//",
+        answerB: "/* */",
+        answerC: "<!-- -->",
+        answerD: "<?-- --!>",
+        correctAnswer: "C"
+    },
+
+    {
+        question: "Which HTML element is at the top of the document and is not visible on the loaded page?",
+        answerA: "main",
+        answerB: "header",
+        answerC: "body",
+        answerD: "title",
+        correctAnswer: "B"
+    },
+
+    {
+        question: "Which of the following elements is NOT a block level element?",
+        answerA: "<img>",
+        answerB: "<p>",
+        answerC: "<div>",
+        answerD: "<ol>",
+        correctAnswer: "A"
+    },
+
+    {
+        question: "What element do you use to link from one page to another?",
+        answerA: "<br></br>",
+        answerB: "<a></a>",
+        answerC: "<title></title>",
+        answerD: "None of these",
+        correctAnswer: "B"
+    },
+
+    {
+        question: "Which of the following is not 100% necessary for a HTML page?",
+        answerA: "<html>",
+        answerB: "<title>",
+        answerC: "<nav>",
+        answerD: "<body>",
+        correctAnswer: "C"
+    },
+
+    { 
+        question: "What HTML element is used to specify a footer for a codument or section?",
+        answerA: "<bottom>",
+        answerB: "<end>",
+        answerC: "<base>",
+        answerD: "<footer>",
+        correctAnswer: "D"
+    },
+
+    {
+        question: "If an image is unable to load, what do you use for an alternative writing?",
+        answerA: "alt",
+        answerB: "text",
+        answerC: "alternative",
+        answerD: "backup",
+        correctAnswer: "A"
+    }
+]
+

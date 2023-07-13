4bda53f3c6aec8492a4aff343fc13b6ebf452f10
dmmontoto <dmontoto@ucsd.edu>
2023-07-12 17:16:20 -0700

    Created Javascript quiz without timer

--- assets/script.js
+++ assets/script.js
@@ -3,92 +3,251 @@
 const questions = [
     {
         question: "What does HTML stand for?",
-        answerA: "Hyper Text Markup Language",
-        answerB: "Hyper Tool Markup Language",
-        answerC: "Home Text Modern Language",
-        answerD: "High Tech Manipulation Language",
-        correctAnswer: "A"
+        optionA: "Hyper Text Markup Language",
+        optionB: "Hyper Tool Markup Language",
+        optionC: "Home Text Modern Language",
+        optionD: "High Tech Manipulation Language",
+        correctOption: "optionA"
     },
 
     {
         question: "What is the correct HTML element for the largest heading?",
-        answerA: "<p>",
-        answerB: "<title>",
-        answerC: "<h1>",
-        answerD: "<h*>",
-        correctAnswer: "C"
+        optionA: "p",
+        optionB: "title",
+        optionC: "h1",
+        optionD: "h*",
+        correctOption: "optionC"
     },
 
     {
         question: "How can you make a bulleted list in HTML?",
-        answerA: "<ol>",
-        answerB: "<bp>",
-        answerC: "<a>",
-        answerD: "<ul>",
-        correctAnswer: "D",
+        optionA: "ol",
+        optionB: "bp",
+        optionC: "a",
+        optionD: "ul",
+        correctOption: "optionD",
     },
 
     {
         question: "What is the correct way to create HTML in-line comments?",
-        answerA: "//",
-        answerB: "/* */",
-        answerC: "<!-- -->",
-        answerD: "<?-- --!>",
-        correctAnswer: "C"
+        optionA: "//",
+        optionB: "/* */",
+        optionC: '"<!-- -->"',
+        optionD: "?-- --!",
+        correctOption: "optionC"
     },
 
     {
         question: "Which HTML element is at the top of the document and is not visible on the loaded page?",
-        answerA: "main",
-        answerB: "header",
-        answerC: "body",
-        answerD: "title",
-        correctAnswer: "B"
+        optionA: "main",
+        optionB: "header",
+        optionC: "body",
+        optionD: "title",
+        correctOption: "optionB"
     },
 
     {
         question: "Which of the following elements is NOT a block level element?",
-        answerA: "<img>",
-        answerB: "<p>",
-        answerC: "<div>",
-        answerD: "<ol>",
-        correctAnswer: "A"
+        optionA: "img",
+        optionB: "p",
+        optionC: "div",
+        optionD: "ol",
+        correctOption: "optionA"
     },
 
     {
         question: "What element do you use to link from one page to another?",
-        answerA: "<br></br>",
-        answerB: "<a></a>",
-        answerC: "<title></title>",
-        answerD: "None of these",
-        correctAnswer: "B"
+        optionA: "br",
+        optionB: "a",
+        optionC: "title",
+        optionD: "None of these",
+        correctOption: "optionB"
     },
 
     {
         question: "Which of the following is not 100% necessary for a HTML page?",
-        answerA: "<html>",
-        answerB: "<title>",
-        answerC: "<nav>",
-        answerD: "<body>",
-        correctAnswer: "C"
+        optionA: "html",
+        optionB: "title",
+        optionC: "nav",
+        optionD: "body",
+        correctOption: "optionC"
     },
 
     { 
         question: "What HTML element is used to specify a footer for a codument or section?",
-        answerA: "<bottom>",
-        answerB: "<end>",
-        answerC: "<base>",
-        answerD: "<footer>",
-        correctAnswer: "D"
+        optionA: "bottom",
+        optionB: "end",
+        optionC: "base",
+        optionD: "footer",
+        correctOption: "optionD"
     },
 
     {
         question: "If an image is unable to load, what do you use for an alternative writing?",
-        answerA: "alt",
-        answerB: "text",
-        answerC: "alternative",
-        answerD: "backup",
-        correctAnswer: "A"
+        optionA: "alt",
+        optionB: "text",
+        optionC: "alternative",
+        optionD: "backup",
+        correctOption: "optionA"
     }
 ]
 
+let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
+
+function handleQuestions() { 
+    //function to shuffle and push 10 questions to shuffledQuestions array
+//app would be dealing with 10questions per session
+    while (shuffledQuestions.length <= 9) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+
+let questionNumber = 1 //holds the current question number
+let playerScore = 0  //holds the player score
+let wrongAttempt = 0 //amount of wrong answers picked by player
+let indexNumber = 0 //will be used in displaying next question
+
+// function for displaying next question in the array to dom
+//also handles displaying players and quiz information to dom
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
+
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctOption = option.labels[0].id
+        }
+    })
+
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++ //adding to player's score
+            indexNumber++ //adding 1 to index so has to display next question..
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++ //adds 1 to wrong attempts 
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer() //check if player picked right or wrong option
+    unCheckRadioButtons()
+    //delays next question displaying for a second just for some effects so questions don't rush in on player
+    setTimeout(() => {
+        if (indexNumber <= 9) {
+//displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+    // condition check for player remark and remark color
+    if (playerScore <= 3) {
+        remark = "Bad Grades, Keep Practicing."
+        remarkColor = "red"
+    }
+    else if (playerScore >= 4 && playerScore < 7) {
+        remark = "Average Grades, You can do better."
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 7) {
+        remark = "Excellent, Keep the good work going."
+        remarkColor = "green"
+    }
+    const playerGrade = (playerScore / 10) * 100
+
+    //data to display to score board
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex"
+
+}
+
+//closes score modal, resets game and reshuffles questions
+function closeScoreModal() {
+    questionNumber = 1
+    playerScore = 0
+    wrongAttempt = 0
+    indexNumber = 0
+    shuffledQuestions = []
+    NextQuestion(indexNumber)
+    document.getElementById('score-modal').style.display = "none"
+}
+
+//function to close warning modal
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
+}
